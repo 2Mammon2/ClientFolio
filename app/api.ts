d0fae153f5dbfb534d7dfeb5d8c6e147c2a765e0
2Mammon2 <nguyentrunghieu842001@gmail.com>
2025-06-19 18:13:02 +0700

    Update api.ts

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://serverfolio-production.up.railway.app/api",
+	baseURL: "https://serverfolio.onrender.com/",
 	headers: {
 		"Content-Type": "application/json",
 	},
